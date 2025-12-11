64f9ec23d421a73aa030ff430e82d9fc1cfd79fd
Matts Lindström <matts.lindstrom@abm.uu.se>
2025-12-11 02:21:14 +0100

    initial commit of gemini npm/tailwind conversion

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./script.js"],
+    theme: {
+        extend: {
+            colors: {
+                background: "#f5f4f2",
+                "fg-grey": "rgba(158, 148, 128, 0.29)",
+                "bg-blend": "rgba(245, 244, 242, 0.5)",
+                "hover-highlight": "rgb(200 201 177 / 15%)",
+            },
+            fontFamily: {
+                mono: ['"IBM Plex Mono"', "monospace"],
+                serif: ['"IBM Plex Serif"', "serif"],
+                sans: ['"IBM Plex Sans"', "sans-serif"],
+            },
+            fontSize: {
+                "7xl-custom": "7.2em", // Matching .large
+            },
+        },
+    },
+    plugins: [],
+};
